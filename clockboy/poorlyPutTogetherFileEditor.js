b7d26bf742578c55eb434906ec246a6dec1a50ca
DEMONITIZED BOI <dexterateed@gmail.com>
2026-05-30 22:21:07 -0700

    add new dev lol

--- clockboy/poorlyPutTogetherFileEditor.js
+++ clockboy/poorlyPutTogetherFileEditor.js
@@ -7,8 +7,7 @@ let numEdited = 0;
 const { Utils } = require("./utils.js");
 
 logInfo();
-addAssociate("crdhdr", "DEVELOPER");
-addAssociate("ReflectiveTape", "DEVELOPER");
+addAssociate("_Xerxes", "DEVELOPER");
 
 /* Thanks to null.
  * you know who you are :3 */
